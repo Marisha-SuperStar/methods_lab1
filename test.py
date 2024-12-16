995fe7991654a5554a07ad903442a1bf6cf586ba
Marisha <berezinamasha23@mail.ru>
2024-12-17 02:16:30 +1000

    Добавили парсинг сайта Hobby Games

--- test.py
+++ test.py
@@ -1,52 +1,51 @@
 import customtkinter
+import requests
+from bs4 import BeautifulSoup
 
-def update_label(content):  # обновляем текст
-    label.configure(text=content)
+def get_description(url):
 
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        desc = soup.find_all("div", class_="desc-text")
 
-def button_callback1():  # тык на кнопку 1
-    with open('base1.txt', 'r', encoding='utf-8') as file:
-        content = file.read()
-        print(content)
-        update_label(content)
+        label.configure(text=desc[0].text.replace('\n', "").replace('\t', '').replace('\r', ''))
+        print(desc[0].text.replace('\n', "").replace('\t', '').replace('\r', ''))
+    else:
+        label.configure(text="Не удалось получить описание игры с сайта. Упси")
 
+url = "https://hobbygames.ru/hardkor"
 
-def button_callback2():  # тык на кнопку 2
-    with open('base2.txt', 'r', encoding='utf-8') as file:
-        content = file.read()
-        print(content)
-        update_label(content)
-
-
-def button_callback3():  # тык на кнопку 3
-    with open('base3.txt', 'r', encoding='utf-8') as file:
-        content = file.read()
-        print(content)
-        update_label(content)
+response = requests.get(url)
+result = {}
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    games = soup.find_all("div", class_="product-card-title")
+    for game in games:
+        result[game.contents[1].attrs['title']] = game.contents[1].attrs['href']
+    print(result)
+else:
+    print(f"Не удалось получить игры с сайта. Упси")
+    exit(-1)
 
 app = customtkinter.CTk()
 app.title("test")
 app.geometry("1000x750")
 
 
-scrollable_frame_left = customtkinter.CTkScrollableFrame(app, width=300, height=700) # создаем фрейм 1
+scrollable_frame_left = customtkinter.CTkScrollableFrame(app, width=300, height=700)
 scrollable_frame_left.grid(row=0, column=0, padx=10, pady=10)
 
+i=0
+for elem in result.keys():
+    button = customtkinter.CTkButton(scrollable_frame_left, text=elem, command=lambda url=result[elem]: get_description(url), width=280, height=50)
+    button.grid(row=i, padx=5, pady=10)
+    i += 1
 
-button = customtkinter.CTkButton(scrollable_frame_left, text="my button", command=button_callback1, width=280, height=50) # создаем кнопку 1
-button.grid(row=0, padx=10, pady=10)
-
-button = customtkinter.CTkButton(scrollable_frame_left, text="your button", command=button_callback2, width=280, height=50) # создаем кнопку 2
-button.grid(row=1, padx=10, pady=10)
-
-button = customtkinter.CTkButton(scrollable_frame_left, text="our button", command=button_callback3, width=280, height=50) # создаем кнопку 3
-button.grid(row=2, padx=10, pady=10)
-
-
-scrollable_frame_right = customtkinter.CTkScrollableFrame(app, width=600, height=700) # создаем фрейм 2
+scrollable_frame_right = customtkinter.CTkScrollableFrame(app, width=600, height=700)
 scrollable_frame_right.grid(row=0, column=1, padx=10, pady=10)
 
-label = customtkinter.CTkLabel(scrollable_frame_right, text="", wraplength=560,  justify="left", font=("Times New Roman", 18)) # выводим текст
+label = customtkinter.CTkLabel(scrollable_frame_right, text="", wraplength=560,  justify="left", font=("Times New Roman", 18))
 label.grid(row=0, column=0, padx=25, pady=25)
 
 app.mainloop()
